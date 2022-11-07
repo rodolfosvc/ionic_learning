e7a0636b9388991556fc3edc5e4ce9d8bfaac1ce
rodolfosvc <rodolfosvc@gmail.com>
2022-11-07 14:30:27 -0300

    feat: add actions in roster page

--- src/app/roster/roster.page.ts
+++ src/app/roster/roster.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 import { Student, StudentsService } from '../students.service';
 
 @Component({
@@ -9,10 +10,53 @@ import { Student, StudentsService } from '../students.service';
 export class RosterPage implements OnInit {
   students: Student[] = [];
 
-  constructor(private studentService: StudentsService) { }
+  constructor(
+    private actionSheetController: ActionSheetController,
+    private studentService: StudentsService) { }
 
   ngOnInit() {
     this.students = this.studentService.getAll();
   }
+
+  async deleteStudent(student: Student) {
+    this.students = this.students
+     .filter(x => x.id !== student.id);
+ }
+
+ async presentActionSheet(student: Student) {
+  const actionSheet = await this.actionSheetController
+  .create({
+    header: `${student.firstName} ${student.lastName}`,
+    buttons: [{
+      text: 'Mark Present',
+      icon: 'eye',
+      handler: () => {
+        student.status = 'present';
+      }
+    }, {
+      text: 'Mark Absent',
+      icon: 'eye-off-outline',
+      handler: () => {
+        student.status = 'absent';
+      }
+    }, {
+      text: 'Delete',
+      icon: 'trash',
+      role: 'destructive',
+      handler: () => {
+        this.deleteStudent(student);
+      }
+    }, {
+      text: 'Cancel',
+      icon: 'close',
+      role: 'cancel',
+      handler: () => {
+        console.log('Cancel clicked');
+      }
+    }]
+  });
+
+  await actionSheet.present();
+}
 
 }
